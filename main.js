a3be19260f76292d5d2f40cb407e64fb2f0d1885
Celine10811020 <shinecloudhill@gmail.com>
2021-09-10 10:59:02 +0800

    Add files via git push

---
+++ main.js
@@ -0,0 +1,17 @@
+function setup()
+{
+  noCanvas();
+
+  /*createP('');
+  var ele_0 = document.createElement("audio");
+  ele_0.src = '歌曲串燒.mp3';
+  ele_0.controls = true;
+  ele_0.autoplay = true;
+  ele_0.loop = true;
+  document.body.appendChild(ele_0);*/
+}
+
+function draw()
+{
+
+}
